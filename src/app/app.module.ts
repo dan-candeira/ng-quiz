cf4f31c212f9a164d791893ee1062f3d71e44149
dan-candeira <danielbarr16@gmail.com>
2022-03-27 19:57:11 -0300

    chore: add quiz component to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, QuizComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
