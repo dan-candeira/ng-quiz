1d565ab1855c9d3c8d725951ebcba78c180d8eb8
Daniel Candeira <danielbarr16@gmail.com>
2022-03-28 18:20:24 -0300

    chore: adjust edit page, add quiz data load

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { QuizService } from '@services/quiz.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EditComponent implements OnInit {
+  quiz: any;
 
-  constructor() { }
+  constructor(
+    private quizService: QuizService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.quizService.loadQuiz(params['id']).subscribe((resp) => {
+        this.quiz = resp;
+      });
+    });
   }
-
 }
